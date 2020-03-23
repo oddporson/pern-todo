ad3de26c16667d3061f1923a6ad2f884a2fc5da5
Porson <porsonlee@gmail.com>
2020-03-22 22:36:05 -0700

    added post route to console log req.body but using postman doesn't seem to be getting any post request

--- server/index.js
+++ server/index.js
@@ -1,10 +1,33 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const pool = require('./db');
 
 //middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); // req.body
+
+// ROUTES
+
+app.post("/todos", async(req,res) => {
+  try {
+    console.log(req.body);
+  } catch (err) {
+    console.error(err.message)
+  }
+  
+})
+
+// Create a todo
+
+// get all todos
+
+// get a todo
+
+// update a todo
+
+// delete a todo
+
 
 app.listen(5000, ()=> {
   console.log("server has started on port 5000");
